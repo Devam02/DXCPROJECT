252fa07d0f61a1c98d5c5f5f9f5c40404cf13bd7
devam <devamoza2@gmail.com>
2020-09-17 16:08:37 +0530

    2ndcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
+import { AuthService } from './auth.service';
+
+import { PlaceorderComponent } from './placeorder/placeorder.component';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ViewOrdersComponent } from './view-orders/view-orders.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    
+    RegisterComponent,
+    AboutComponent,
+    ContactUsComponent,
+    ViewOrdersComponent,
+    ThankYouComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+
+
+     
+      {path:'about',component:AboutComponent},
+      {path:'register' ,component:RegisterComponent},
+      {path:'login',component:LoginComponent},
+      
+      
+    
+      {path:'Logout',component:AboutComponent},
+      {path:'Placeorder',component:PlaceorderComponent},
+      {path:'Vieworders',component:ViewOrdersComponent}
+      
+    ])
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
